ac366ecc3ef7f108a889b5d7fec89d27eed98172
Scobru <scobru1988@gmail.com>
2023-09-16 10:26:24 +0200

    generic fixes

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -13,7 +13,7 @@ export type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The network where your DApp lives in
-  targetNetwork: chains.sepolia,
+  targetNetwork: chains.baseGoerli,
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect on the local network
   pollingInterval: 5000,
