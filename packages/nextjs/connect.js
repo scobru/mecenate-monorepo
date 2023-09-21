1030e09272b2a2a2eefd1fb2705edc33a2789be4
Scobru <scobru1988@gmail.com>
2023-09-21 09:06:35 +0200

    bug fix

--- packages/nextjs/connect.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./chat.db');
-
-db.serialize(() => {
-  db.run("CREATE TABLE IF NOT EXISTS messages (username TEXT, message TEXT, timestamp TEXT)");
-});
-
-module.exports = db;
